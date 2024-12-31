72d9a8cac065725c36aeea1ba16f432a96b07e53
weiweicao <weiwei.cao@mdland.com>
2024-12-30 21:38:26 -0500

    docs: update the hexagram and type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -10,5 +10,7 @@ type TossResult = '三正' | '兩正一反' | '兩反一正' | '三反';
 type Hexagram = {
   code: string; // 六爻對應的二進制代碼，如 111000 由左至右分別代表卦从下至上的六爻，1 代表阳爻，0 代表阴爻
   name: string; // 卦名，例如“乾卦”
-  description: string; // 卦辭
+  description: {
+    [key: string]: string[]; // 卦辭，key 为字符串，值为字符串数组
+  };
 };
